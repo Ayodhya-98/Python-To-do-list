9f5e73565c574e2ee38f07d2f4e362d9c2c0721d
Ayodhya-98 <ayodyanirmanirm@gmail.com>
2023-05-29 14:02:16 +0530

    Start Creating Functions

---
+++ todoapp.py
@@ -0,0 +1,15 @@
+todo_list=[]
+
+def display_menu():
+    print("Simple Todo list")
+    print("1- Display The TODO list")
+    print("2- Add items to TODO list")
+    print("3- Mark item as complete")
+    print("4- Mark item as incomplete")
+    print("5- Delete item")
+    
+    
+    
+
+while True:
+    
